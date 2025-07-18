a3032881c4df2b342b87106a175aa0083640c3cc
Mishal Qamar <mishalqamar@gmail.com>
2025-07-18 14:41:47 +0100

    y

--- src/components/markdown-text.tsx
+++ src/components/markdown-text.tsx
@@ -1,39 +1,45 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-// Import style from cjs path, which has correct type
 import { vscDarkPlus } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 type PostMarkdownProps = {
   content: string;
 };
 
+type CodeProps = {
+  inline?: boolean;
+  className?: string | undefined;
+  children?: React.ReactNode;
+};
+
+const components = {
+  code({ inline, className, children, ...props }: CodeProps) {
+    const match = /language-(\w+)/.exec(className || '');
+
+    if (match && !inline) {
+      return (
+        <SyntaxHighlighter
+          PreTag="div"
+          language={match[1]}
+          style={vscDarkPlus}
+          {...props}
+        >
+          {String(children).replace(/\n$/, '')}
+        </SyntaxHighlighter>
+      );
+    }
+
+    return (
+      <code className={className} {...props}>
+        {children}
+      </code>
+    );
+  },
+};
+
 export const MarkdownText = ({ content }: PostMarkdownProps) => {
   return (
-    <ReactMarkdown
-      components={{
-        code({ className, children, ...rest }) {
-          const match = /language-(\w+)/.exec(className || '');
-          const { ref, ...propsWithoutRef } = rest;
-
-          return match ? (
-            <SyntaxHighlighter
-              PreTag="div"
-              language={match[1]}
-              style={vscDarkPlus as any} // no type assertion needed here
-              {...propsWithoutRef}
-            >
-              {String(children).replace(/\n$/, '')}
-            </SyntaxHighlighter>
-          ) : (
-            <code className={className} {...rest}>
-              {children}
-            </code>
-          );
-        },
-      }}
-    >
-      {content}
-    </ReactMarkdown>
+    <ReactMarkdown components={components}>{content}</ReactMarkdown>
   );
 };
